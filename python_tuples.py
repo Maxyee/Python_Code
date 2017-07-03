7fe484609f670823ff6754c9a5bdea9625ca9aeb
Julhas <mdjulhashossainmohon@gmail.com>
2017-07-04 02:54:39 +0600

    python tuple Data type

---
+++ python_tuples.py
@@ -0,0 +1,11 @@
+#Data Type Tuple
+
+tuple = ( 'abcd', 786 , 2.23, 'john', 70.2 )
+tinytuple = (123, 'john')
+
+print tuple
+print tuple[0]
+print tuple[1:3]
+print tuple[2:]
+print tinytuple * 2
+print tuple + tinytuple
